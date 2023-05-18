9907cd3955f0b96a77c4bafbf7b270572f89a2fe
Andrea Ranica <andrearanica2004@gmail.com>
2023-05-18 14:21:51 +0200

    🚚 Move files

---
+++ old/script.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+    document.getElementById('page').className = "container my-5 text-center show"
+    document.getElementById('navbar').className = "text-center bg-dark show"
+}
