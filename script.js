b3cef8a4995c0a5407873afa99c0092648999681
andrearanica <andrearanica2004@gmail.com>
2023-11-04 17:16:48 +0100

    ✨ Improved everything

--- script.js
+++ script.js
@@ -3,65 +3,33 @@ let response = ''
 let previous = document.getElementById('previous')
 let header = document.getElementById('header')
 
-const education = `
-<div class='row'>
-    <b class='title'>ITIS Paleocapa</b> | Computer science (2018-now)<br>
-    <div class='row'>
-        &nbsp;I have studied computer science at ITIS Paleocapa, in Bergamo<br>&nbsp;Grade: 100/100<br>
-    </div>
-</div>
-`
-
-/*
-<b class='title'>Software development</b><br>
-UML deployment diagrams&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a class='full-star'>★★★★</a>☆<br>
-Entity-relationship diagrams <a class='full-star'>★★★★</a>☆<br>
-C# Windows Form Applications <a class='full-star'>★★★★</a>☆<br>
-C++ and Python languages&nbsp;&nbsp;&nbsp;&nbsp; <a class='full-star'>★★★</a>☆☆<br><br>
-*/
-
-const skills = `
-<div class='row'>
-<b class='title'>Web development</b><br>
-<div class='row'>
-    Frontend: <i class="devicon-html5-plain colored"></i> <i class="devicon-css3-plain colored"></i> <i class="devicon-javascript-plain colored"></i>&nbsp;<i class="devicon-react-original colored"></i> <i class="devicon-bootstrap-plain colored"></i><br>
-    Backend:  &nbsp;<i class="devicon-php-plain colored"></i> <i class="devicon-mysql-plain colored"></i>&nbsp;<i class="devicon-nodejs-plain colored"></i> <i class="devicon-express-original colored"></i> <i class="devicon-mongodb-plain-wordmark"></i>
-</div><br>
-<b class='title'>Software design</b>
-<div class='row'>
-    UML deployment diagram | Project management | Entity-relationship diagrams | Basics of cybersecurity
-</div>
-</div>
-`
-
-const links = `
-<div class='row'>
-    <b class='title'>Reach me!</b><br>
-    <div class='row'>
-        &nbsp;&nbsp;<i class="devicon-linkedin-plain"></i> <a href='https://linkedin.com/in/andrearanica'>Linkedin</a> (@andrearanica)<br>
-        &nbsp;&nbsp;<i class="devicon-github-original"></i> <a href='https://github.com/andrearanica'>GitHub</a> &nbsp;&nbsp;(@andrearanica)<br>
-    </div>
-</div>
-`
-
 const help = `
 GNU bash, version 5.1.4(1)-release (x86_64-pc-linux-gnu)<br>
-<a class='title-red'>Welcome on my portfolio!</a><br>Choose one of the following commands to continue:<br>
-&nbsp;&nbsp;summary&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;See all my learning experiences<br>
+&nbsp;&nbsp;summary&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Who am I?<br>
 &nbsp;&nbsp;skills&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;See all my skills<br>
-&nbsp;&nbsp;education&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;See all my projects<br>
-&nbsp;&nbsp;links&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;See all my projects<br>
-&nbsp;&nbsp;clear&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Clean the screen<br>
 `
 
-const projects = `
-<div class='row'>
-    <b class='title'>Fotoregistro</b> (12/2022 - now)<br>
-    <div class='row'>
-        I am developing a web application to help teachers collecting students' photos <a href='https://github.com/andrearanica/fotoregistro'>(GitHub)</a><br>
-    </div>
+const summary = `
+<center>
+<div style="font-size: 120%;">
+<h2 style='color: orange'>Welcome on my website! I'm Andrea, a Junior Software Developer</h2>
+I'm a Junior Software developer who wants to learn more every single day. I love improving my skills in computer science and programming.<br>
+In my free time, I like going to the gym 🏋🏻, play videogames 🕹️ and improve myself 🚀<br>
+<h3 style='color: orange'>What am I doing? 👀</h3>
+I'm studying Computer Science @ University of Milano-Bicocca (currently first year)</li><br>
+I'm working as a Software developer @ DDX Software Solutions</li><br>
+<h3 style='color: orange'>My skills ✨</h3>
+<img src="https://skillicons.dev/icons?i=python,cs,cpp,javascript,nodejs,php,mysql,mongodb,bash," /><br>
+<img src="https://skillicons.dev/icons?i=bootstrap,react,express,laravel" /><br>
+<img src="https://skillicons.dev/icons?i=linux,vscode,github,gitlab,postman" />
+<br>
+<h3>Connect with me 👋🏻</h3>
+If you want to know more about me or to contact me, use the buttons below. A presto 💪🏻<br><br>
+<a href="https://github.com/andrearanica"><img src="https://skillicons.dev/icons?i=github" /></a>
+<a href="https://linkedin.com/in/andrearanica"><img src="https://skillicons.dev/icons?i=linkedin" /></a>
+<br><br>
 </div>
-`
+</center>`
 
 previous.innerHTML += `<a class='dollar'>user@andrearanica:~$</a> help<br>${ help }<br>`
 
@@ -70,60 +38,25 @@ document.getElementById('input-form').addEventListener('submit', (e) => {
     response = ''
     switch (input.value) {
         case 'help': 
-            response = `
-            ${ help }
-            `
+            response = help
             break;
         case 'summary':
-            response = `
-            <div class='row'>
-                <h3>Hi everyone! I am Andrea, a Junior Software Developer and Computer science student!</h3>
-                <h3 class='title-red'>Education</h3><br>
-                ${ education }<br>
-                <h3 class='title-red'>Projects</h3><br>
-                ${ projects }<br>
-                <h3 class='title-red'>Skills</h3><br>
-                ${ skills }<br>
-                <h3 class='title-red'>Social</h3><br>
-                ${ links } <br> 
-            </div>
-            `
-            break;
-        case 'education':
-            response = `
-            ${ education }
-            `
-            break
-        case 'skills':
-            response = `
-            ${ skills }
-            ` 
-            break
-        case 'projects':
-            response = `
-            ${ projects }
-            `
-            break
-        case 'links':
-            response = `
-            ${ links }
-            `
-            break
-        case 'clear':
+            response = summary
+            break   
+        case 'cls':
             previous.innerHTML = ''
             break
         case 'ls':
             response = `
-            index.html&nbsp;&nbsp;&nbsp;script.js&nbsp;&nbsp;&nbsp;style.css
+            index.html&nbsp;&nbsp;&nbsp;script.js&nbsp;&nbsp;&nbsp;style.css<br>
             `
             break
-        case 'pwd':
-            response = 'https://andrearanica.altervista.org/'
-            break
         default: 
-            response = `bash: ${ input.value }: command not found`
+            response = `bash: ${ input.value }: command not found<br>`
             break
     }
-    if (input.value !== 'clear') { previous.innerHTML += `<a class='dollar'>user@andrearanica:~$</a> ${ input.value }<br>${ response }<br>` }
+    if (input.value !== 'cls') { 
+        previous.innerHTML += `<a class='dollar'>user@andrearanica:~$</a> ${ input.value }<br>${response}`
+    }
     input.value = ''
 })
