9a816b8512584c160efa11964e528e320df07c0f
Andrea Ranica <andrearanica2004@gmail.com>
2024-07-16 09:40:40 +0200

    Started refactoring

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let input = document.getElementById('input')
+let inputCommand = document.getElementById('input-command')
+let inputForm = document.getElementById('input-form')
 let response = ''
 let previous = document.getElementById('previous')
 let header = document.getElementById('header')
@@ -10,69 +11,64 @@ GNU bash, version 5.1.4(1)-release (x86_64-pc-linux-gnu)<br>
 &nbsp;&nbsp;clear&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Clear the screen<br>
 `
 
-const summary = `
-<center>
-<div style="font-size: 120%;">
-<h2 style='color: orange'>Welcome on my website! I'm Andrea, a Junior Software Developer</h2>
-I'm a Junior Software developer who wants to learn more every single day. I love improving my skills in computer science and programming.<br>
-In my free time, I like going to the gym 🏋🏻, play videogames 🕹️ and improve myself 🚀<br>
-<h3 style='color: orange'>What am I doing? 👀</h3>
-I'm studying Computer Science @ University of Milano-Bicocca (currently first year)</li><br>
-I'm working as a CAD/CAM Software developer @ DDX Software Solutions</li><br>
-<h3 style='color: orange'>My skills ✨</h3>
-<img src="https://skillicons.dev/icons?i=python,cs,cpp,javascript,nodejs,php,mysql,mongodb,bash," /><br>
-<img src="https://skillicons.dev/icons?i=bootstrap,react,express,laravel" /><br>
-<img src="https://skillicons.dev/icons?i=linux,vscode,github,gitlab,postman" />
-<br>
-<h3 style='color: orange'>Connect with me 👋🏻</h3>
-If you want to contact me, use the buttons below... I'll respond you as soon as possible 💪🏻<br><br>
-<a href="https://github.com/andrearanica"><img src="https://skillicons.dev/icons?i=github" /></a>
-<a href="https://linkedin.com/in/andrearanica"><img src="https://skillicons.dev/icons?i=linkedin" /></a>
-<a href="https://linkedin.com/in/andrearanica"><img src="https://skillicons.dev/icons?i=gmail" /></a>
-<br><br>
-</div>
-</center>`
-
-const projects = `
-<center>
-<div style="font-size: 120%">
-<h2 style="color: orange">Projects</h2>
-Here there are some of the projects I made during the last years to learn some of the key topics of computer science    
-<li><a href="https://github.com/andrearanica/piantapp">PiantApp</a>: a social media made with sockets with client/server architecture</li>
-<li><a href="https://github.com/andrearanica/blockchain">Blockchain</a>: a simulator of the blockchain technology made in JavaScript</li>
-<li><a href="https://github.com/andrearanica/fotoregistro">Fotoregistro</a>: a web application for teachers and students</li>
-</div></center><br>`
-
-previous.innerHTML += `<a class='dollar'>user@andrearanica:~$</a> help<br>${ help }<br>`
-
-document.getElementById('input-form').addEventListener('submit', (e) => {
+inputForm.addEventListener('submit', e => {
     e.preventDefault()
-    response = ''
-    switch (input.value) {
-        case 'help': 
-            response = help
-            break;
-        case 'resumee':
-            response = summary
+    command = inputCommand.value
+    switch (command) {
+        case 'help':
+            writeHelp()
             break
-        case 'projects':
-            response = projects
-            break   
-        case 'clear':
-            previous.innerHTML = ''
-            break
-        case 'ls':
-            response = `
-            index.html&nbsp;&nbsp;&nbsp;script.js&nbsp;&nbsp;&nbsp;style.css<br>
-            `
-            break
-        default: 
-            response = `bash: ${ input.value }: command not found<br>`
+        default:
+            writeError(command)
             break
     }
-    if (input.value !== 'clear') { 
-        previous.innerHTML += `<a class='dollar'>user@andrearanica:~$</a> ${ input.value }<br>${response}`
-    }
-    input.value = ''
-    window.scrollTo(0, document.body.scrollHeight)
+    writeInputLine()
+    inputCommand.value = ''
 })
+
+const writeMain = async () => {
+    let title = `I'm Andrea, a Software Developer & Computer Science student`
+    writeTitle(title)
+    let work = `I work as a Software Developer @ DDX Software Solutions`
+    let university = `I'm studying Computer Science @ University of Milano-Bicocca`
+    await write(work)
+    await write(university)
+}
+
+const writeTitle = text => {
+    previous.innerHTML += `<h2 style='color: orange; text-align: center; font-size: 200%;'>${text}</h2>`
+}
+
+const writeSubtitle = text => {
+    previous.innerHTML += `<h2 style='color: orange; text-align: center;'>${text}</h2>`
+}
+
+const writeHelp = async () => {
+    // TODO add constants instead of strings
+    await write('GNU bash, version 5.1.4(1)-release (x86_64-pc-linux-gnu)')
+}
+
+const writeError = async (wrongCommand) => {
+    errorMessage = `bash: ${wrongCommand}: command not found`
+    await write(errorMessage)
+}
+
+const write = async (message) => {
+    i = 0
+    clearInterval()
+    setInterval(() => {
+        if (i < message.length) {
+            previous.innerHTML += message.charAt(i)
+            i++
+        } else {
+            clearInterval()
+        } 
+    }, 10)
+    previous.innerHTML += '<br>'
+}
+
+const writeInputLine = () => {
+    previous.innerHTML += `<a class='dollar'>user@andrearanica:~$</a> ${ inputCommand.value }<br></br>`
+}
+
+writeMain()
